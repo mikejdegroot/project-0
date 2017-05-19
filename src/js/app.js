c3aa43b12c60f08e8761a812c30c1047451f0e67
Mike de Groot <mike.j.degroot@gmail.com>
2017-05-19 14:34:57 +0100

    added keylisten audio functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log('JS Loaded');
+$(() => {
+  console.log('JS yo');
+
+
+  const audio = $('audio')[0];
+  const $document = $(document);
+
+
+//assigns the audio src according to keypress id of keys 1-8 BOOM!
+  $document.keypress(function(e) {
+    console.log(e.charCode);
+    audio.src = `audio/${e.charCode}.wav`;
+    audio.play();
+  });
+
+
+
+
+
+
+
+
+});
