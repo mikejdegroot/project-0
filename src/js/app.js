000ed01ab8bfb408e462c4343a74b13b652b293f
Mike de Groot <mike.j.degroot@gmail.com>
2017-05-21 13:09:55 +0100

    added increasing rounds logic

--- src/js/app.js
+++ src/js/app.js
@@ -8,21 +8,19 @@ $(() => {
   const $sounds = ['49', '50','51','52','53','54','55','56'];
 
 
-  // current ROUND DISPLAY. empty programSequence array,
+  // current round. empty programSequence array,
   //gets filled x3 at start then (needs to be + 1 or 2 per round)!!!!
   let currentRound = 3;
   let programSequence = [];
   let userSequence = [];
   let compare = [];
-  let count = 0;
-
 
 
   //Playing yes/ no switch
   let playing = false;
 
 
-  //assigns the audio src according to keypress id of keys 1-8 BOOM!
+  //assigns the audio src according to keypress id of keys 1-8.
   $document.keypress(function(e) {
     console.log(e.charCode);
     audio.src = `audio/${e.charCode}.wav`;
@@ -35,41 +33,39 @@ $(() => {
   $game.on('click', () => {
     $welcome.hide();
     playing = true;
-    console.log(playing);
     gameInit();
   });
 
   //initializes the building of the program array
   function gameInit () {
     for (let i = 0; i < currentRound; i++){
-      console.log('initializing!');
       const rand = (Math.floor(Math.random()*8));
       programSequence.push($sounds[rand]);
     }
+    console.log('initializing!');
     playBack();
   }
 
 
   // here does there need to be a game add random addition to array OR a way of using the above function just to add one?
 
-  //this plays back the program array but all at once at the moment, needs time delay asap
+  //this plays back the constructed program array
   let timerid = 0;
 
 
   function playBack() {
     setTimeout(function () {
-      // console.log('Hello');
       audio.src = `audio/${programSequence[timerid]}.wav`;
       audio.play();
       timerid++;
       if (timerid < currentRound) {
         playBack();
       } else {
-        listen();
+
         timerid = 0;
         console.log(programSequence);
       }
-    }, 1500);
+    }, 1000);
   }
 
   //Function pushes key charcodes the user inputs to the userSequence array.
@@ -77,86 +73,33 @@ $(() => {
     console.log('listening...');
     $document.keypress(function(e) {
       userSequence.push(e.charCode.toString());
-      // console.log(userSequence);
+      console.log(userSequence);
       compareArrays();
     });
 
   }
   //
+  listen();
+
+
+  let count  = 0;
   // compares the two arrays when the length matches the current round. at the moment converts the number to string using ==, which is not ideal but it works
   function compareArrays() {
-    // if (userSequence.length === programSequence.length){
-    //   for (let i = 0; i <programSequence.length; i++){
-    //     if (programSequence[i] == userSequence[i]){
-    //       compare.push('win');
-    //     }else {
-    //       compare.push('loose');
-    //       // console.log(compare);
-    //     }
-    //   }
-    // }
-    // function isWin(element, index, array) {
-    //   return element === 'win';
-    // }
-    //console.log(compare.every(isWin));
+    console.log('yo'+count);
+
+    const theSame = userSequence.length === programSequence.length && userSequence.every((v,i) => v === programSequence[i]);
     count += 1;
-    const theSame = userSequence.length === programSequence.length && userSequence.every((v,i)=> v === programSequence[i]);
     if (count === currentRound) {
       console.log(theSame);
       if (theSame === true) {
-        const rand = Math.floor(Math.random());
+        let rand = Math.floor(Math.random()*8);
         programSequence.push($sounds[rand]);
-        currentRound += 1;
-        clearInterval(timerid);
+        userSequence = [];
+        count = 0;
         playBack();
-
+        currentRound += 1;
       }
-    }// return theSame;
+    }
   }
-
-    // function nextRound () {
-    //   if (theSame === true) {
-    //     const rand = Math.floor(Math.random());
-    //     programSequence.push($sounds[rand]);
-    //   }
-    // }
-
-  // attach the .equals method to Array's prototype to call it on any array
-  // Array.prototype.equals = function (array) {
-  //   // if the other array is a falsy value, return
-  //   if (!array)
-  //     return false;
-  //
-  //   // compare lengths - can save a lot of time
-  //   if (this.length !== array.length)
-  //     return false;
-  //
-  //   for (var i = 0, l=this.length; i < l; i++) {
-  //     // Check if we have nested arrays
-  //     if (this[i] instanceof Array && array[i] instanceof Array) {
-  //       // recurse into the nested arrays
-  //       if (!this[i].equals(array[i]))
-  //         return false;
-  //     } else if (this[i] !== array[i]) {
-  //       // Warning - two different object instances will never be equal: {x:20} != {x:20}
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // };
-  // // Hide method from for-in loops
-  // Object.defineProperty(Array.prototype, 'equals', {enumerable: false});
-  //
-  // console.log(programSequence.equals(userSequence));
-
-
-
-
-  //
-  // function winner(element, index, compare) {
-  //   return element = 'win';
-  // }
-  //
-  // compare.every(winner);
 
 });
