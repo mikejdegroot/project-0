7af5dbd1f0d32fe298ccc0dd33df64182f38034f
Mike de Groot <mike.j.degroot@gmail.com>
2017-05-23 15:56:05 +0100

    playground mode added, debugged to run even after game mode has been exited

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ $(() => {
   const audio = $('audio')[0];
   const $welcome = $('.welcome');
   const $game = $('#game');
+  const $playground = $('#playground');
   const $feedback = $('.feedback');
   const $reset = $('.reset');
   const $endGame = $('.endgame')
@@ -78,6 +79,7 @@ $(() => {
 
 
   //Playing yes/ no switch
+  let messing = false;
   let playing = false;
 
 
@@ -86,8 +88,10 @@ $(() => {
 
   //assigns the audio src according to keypress id of keys 1-8.
   $document.keypress(function(e) {
-    const pattern = visuals[`${e.charCode}`];
-    visualise(pattern);
+    if ((playing === true) || (messing === true)) {
+      const pattern = visuals[`${e.charCode}`];
+      visualise(pattern);
+    }
   });
 
 
@@ -99,6 +103,12 @@ $(() => {
     $feedback.removeClass('hidden');
     $feedback.html(currentPlayer);
     setTimeout(gameInit, 3000);
+  });
+
+  $playground.on('click', () => {
+    $welcome.hide();
+    messing = true;
+    $endGame.removeClass('hidden');
   });
 
   function updatePlayer () {
@@ -146,7 +156,7 @@ $(() => {
   }
 
   function visualise (pattern) {
-    $feedback.html(currentPlayer);
+    // $feedback.html(currentPlayer);
     audio.src = `audio/${pattern.audio}.wav`;
     audio.play();
     const $element = pattern.element.clone();
@@ -161,6 +171,7 @@ $(() => {
 
   //Function pushes key charcodes the user inputs to the userSequence array.
   function listen() {
+    // if (playing === true) {
     console.log('listening...');
     $document.keypress(function(e) {
       userSequence.push(e.charCode);
@@ -169,7 +180,7 @@ $(() => {
     });
 
   }
-  //
+
   listen();
 
   let losses = 0;
@@ -183,7 +194,7 @@ $(() => {
   function compareArrays() {
     const theSame = userSequence.length === programSequence.length && userSequence.every((v,i) => v === programSequence[i].audio);
     count += 1;
-    if ((count === currentRound) && (losses === 0)){
+    if ((count === currentRound) && (losses === 0) && (messing !== true)) {
       console.log(theSame);
       if (theSame === true) {
         const rand = (Math.floor(Math.random()*8));
@@ -197,7 +208,7 @@ $(() => {
         $feedback.html('Pass! - next up ' + currentPlayer);
         $feedback.removeClass('hidden');
         setTimeout(playBack, 3000);
-      } else if (theSame === false) {
+      } else if ((theSame === false) && (messing !== true)) {
         userSequence = [];
         count = 0;
         losses += 1;
@@ -211,12 +222,12 @@ $(() => {
 
         //this bottom part of the if else acts as the sudden death calculator portion of the game, works on whether a loss was stored in the 'losses' var.
       }
-    } else if ((count === currentRound) && (losses !== 0)){
+    } else if ((count === currentRound) && (losses !== 0) && (messing !== true)){
       if (theSame === true) {
         $feedback.html(currentPlayer + ' Wins!');
         $feedback.removeClass('hidden');
-        $reset.removeClass('hidden');
-      } else if (theSame === false) {
+        $endGame.removeClass('hidden');
+      } else if ((theSame === false) && (messing !== true)) {
         $feedback.html(currentPlayer + ' Fails!! ---- TIE!');
         $feedback.removeClass('hidden');
         $endGame.removeClass('hidden');
@@ -231,6 +242,7 @@ $(() => {
     programSequence = [];
     userSequence = [];
     playing = false;
+    messing = false;
     updatePlayer();
     losses = 0;
     count = 0;
